b6139ddede5782f5e686c2aa90d691750367dd19
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-01-14 15:48:32 +0100

    index.php

---
+++ jour01/job 02/index.php
@@ -0,0 +1,10 @@
+<?php
+ $nombre_de_lignes= "1";
+
+ while ($nombre_de_lignes<=2)
+ {echo"Hello la Plateforme<br/>";//Ceci est une ligne  /Ceci est un bloc /
+
+    $nombre_de_lignes++;//$nombre_de_lignes=$nombre de lignes + 1
+
+}
+?>
