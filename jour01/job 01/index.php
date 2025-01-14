08bc29502687163c831f78dfa833fd61cf741b3e
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-01-14 14:32:01 +0100

    job 01

---
+++ jour01/job 01/index.php
@@ -0,0 +1,17 @@
+<?php
+$str ="La Plateforme";
+echo $str;
+$str2 ="Vive";
+$str3="!";
+echo "$str2 $str $str3";
+
+$val="6";
+echo $val;
+echo $val+"4";
+
+$myBool="true";
+echo $myBool;
+
+$myBool="false";
+echo $myBool;
+
