85941e90e465223375a9f61b939711e5053de4c5
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-01-17 15:52:32 +0100

    enlever nombres

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,10 +1,10 @@
 <?php
-$exclude_numbers=  [26,37,88,1111];
+
 
 for ($i = 1 ; $i <= 1337 ; $i++){
    if ($i == 26 or $i == 37 or $i = 88 or $i =1111){ 
    
-    unset($exclude_numbers[$i]);
+    echo "<br> <br\>";
  }
  
  else {
