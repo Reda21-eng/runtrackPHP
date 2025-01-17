aefbf9c0931366bd1b1bdcb0edfa3ebfb941e6b0
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-01-17 16:42:41 +0100

    enlever nombre

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -2,15 +2,17 @@
 
 
 for ($i = 1 ; $i <= 1337 ; $i++){
-   if ($i == 26 or $i == 37 or $i = 88 or $i =1111){ 
+   if ($i == 26 or $i == 37 or $i = 88 or $i =1111){
+      
+      echo "<br> <br\>";
+   } else {
+      echo"<br>$i<br\>";
+   }
+}
    
-    echo "<br> <br\>";
- }
+
  
-   else {
-    echo"<br>$i<br\>";
- };
-};
+ 
 
 ?>
 
