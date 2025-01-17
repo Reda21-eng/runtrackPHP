efabacef55979112ad2ad7aa3fee1b5fcdb4acc2
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-01-17 15:34:16 +0100

    exclude numbers

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,8 +1,8 @@
 <?php
-$exclude_numbers = [26,37,88,11];
+$exclude_numbers=  [26,37,88,1111];
 
 for ($i = 1 ; $i <= 1337 ; $i++){
-   if ($i == $exclude_numbers){
+   if ($i == 26 or $i == 37 or $i = 88 or $i =1111){ 
    
     unset($exclude_numbers[$i]);
  }
