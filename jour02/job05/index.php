6047ca97eb26f809a2681664efdcb98bd3aa2bd1
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-01-20 13:42:46 +0100

    job05

---
+++ jour02/job05/index.php
@@ -0,0 +1,17 @@
+<?php
+
+for ($i = 0; $i <=1000; $i++) {
+    if ($i % $i == 0) {
+        echo "<br>". "ne pas afficher "."</br>";
+    }
+
+    else {
+        echo "<br>".$i."</br>";
+    }
+
+}
+
+?>
+
+
+       
